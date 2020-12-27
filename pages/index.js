7814897c15779bd77390c109864f54d03416817a
endziu <koper.andrzej@gmail.com>
2020-12-27 21:46:30 +0100

    moar emoji

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,9 @@ export default function IndexPage() {
         </div>
       </nav>
       <div className="bg-gray-100 dark:bg-gray-800 flex" style={{"minHeight": "calc(100vh - 26px - 32px)"}}>
-        <div className="bg-gray-100 dark:bg-gray-700 m-auto p-10 shadow-lg rounded-2xl text-gray-300 no-underline">
-          <span className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl align-middle"> 🦄🎄🎅🎁</span>
+        <div className="flex flex-col bg-gray-100 dark:bg-gray-700 m-auto p-8 shadow-lg rounded-2xl text-gray-300 no-underline">
+          <span className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl align-middle leading-snug md:leading-snug lg:leading-snug xl:leading-snug"> 🧭🗺️🦄🎄🎅🎁</span>
+          <span className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl align-middle leading-snug md:leading-snug lg:leading-snug xl:leading-snug"> ❄️❤️✨🍪🚧⏳</span>
         </div>
       </div>
     </div>
