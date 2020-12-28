c94f346841861f80789428280a8832d84e68cdaf
endziu <koper.andrzej@gmail.com>
2020-12-28 17:14:43 +0100

    refactor components

---
+++ components/themeSwitch.js
@@ -0,0 +1,28 @@
+export default function ThemeSwitch({handler, current}) {
+  return(
+    <div className="flex items-center justify-end">
+      <label
+        htmlFor="toggle"
+        className="text-xs text-gray-600 dark:text-gray-100 mr-2 cursor-pointer"
+      >
+        dark mode
+      </label>
+      <div
+        className="relative inline-block w-10 align-middle select-none transform transition-transform duration-500 ease-in"
+      >
+        <input
+          type="checkbox"
+          name="toggle"
+          id="toggle"
+          onChange={handler}
+          checked={current === "dark"}
+          className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer transition-transform duration-300"
+        />
+        <label 
+          htmlFor="toggle"
+          className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer transition-colors duration-500"
+        />
+      </div>
+    </div>
+  )
+}
