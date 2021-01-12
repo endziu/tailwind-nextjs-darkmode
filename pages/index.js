fd914b9242de76476f08d8f3a0740969a7686a7a
endziu <koper.andrzej@gmail.com>
2021-01-12 16:46:26 +0100

    fix borders

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,15 @@ function IndexPage() {
         className="flex flex-col md:flex-row transition-colors duration-500"
       >
 
-        <div className={`block md:hidden ${state.menu ? 'h-1/3' : 'h-0'} w-full md:h-full md:w-1/3 bg-gray-200 dark:bg-gray-900 border-b border-gray-400 md:border-r md:border-b-0 text-3xl text-gray-900 dark:text-gray-100 transition-translate duration-500`}>
-          menu
+        <div className={`block md:hidden ${state.menu ? 'h-1/3' : 'h-0'} w-full md:h-full md:w-1/3 bg-gray-200 dark:bg-gray-900 ${state.menu ? 'border-b border-gray-400 md:border-r md:border-b-0' : ''} text-3xl text-gray-900 dark:text-gray-100 transition-translate duration-500`}>
+          <span className="ml-4">menu</span>
         </div>
-        <div className={`hidden md:block h-1/3 w-full md:h-full ${state.menu ? 'md:w-1/3' : 'md:w-0'} bg-gray-200 dark:bg-gray-900 border-b border-gray-400 md:border-r md:border-b-0 text-3xl text-gray-900 dark:text-gray-100 transition-translate duration-500`}>
-          menu
+        <div className={`hidden md:block h-1/3 w-full md:h-full ${state.menu ? 'md:w-1/3' : 'md:w-0'} bg-gray-200 dark:bg-gray-900 ${state.menu ? 'border-b border-gray-400 md:border-r md:border-b-0' : ''} text-3xl text-gray-900 dark:text-gray-100 transition-translate duration-500`}>
+          <span className="ml-4">menu</span>
         </div>
 
-        <div className={`${state.menu ? 'h-2/3' : 'h-full'} md:h-full ${state.menu ? 'md:w-2/3' : 'md:w-full'} text-3xl bg-gray-200 dark:bg-gray-900 border-b border-gray-400 md:border-r md:border-b-0 text-gray-900 dark:text-gray-100 transition-translate duration-500`}>
-          main
+        <div className={`${state.menu ? 'h-2/3' : 'h-full'} md:h-full ${state.menu ? 'md:w-2/3' : 'md:w-full'} text-3xl bg-gray-200 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-translate duration-500`}>
+          <span className="ml-4">main</span>
         </div>
 
       </main>
